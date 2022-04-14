d2bbb3b7512272264202efaeb0a401fac3185004
MosesDerProphet <99213944+PatderCoole@users.noreply.github.com>
2022-04-14 21:47:56 +0200

    Springen
    Wir haben das Springen gelernt und perfektioniert! - MosesDerProphet

---
+++ Assets/Scripts/Ground_Check.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground_Check : MonoBehaviour
+{
+
+    private bool Is_touching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Is_touching = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Is_touching = false;
+    }
+
+    public bool IsTouch_State()
+    {
+        return Is_touching;
+    }
+}
--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
 
-    public float movement_speed = 2f;
+    private Rigidbody rb;
+    public float movement_speed = 5f;
+    public float jumping_speed = 14000f;
+    public Ground_Check luca;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -31,6 +34,17 @@ public class Player_Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(new Vector3(-1.0f, 0.0f, 0.0f) * Time.deltaTime * movement_speed);
+        }
+
+
+        
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && luca.IsTouch_State() == true)
+        {
+            //transform.Translate(new Vector3(0.0f, 1.0f, 0.0f) * Time.deltaTime * jumping_speed);
+            rb.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * jumping_speed);
         }
     }
 }
