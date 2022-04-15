7b2f9663f8d5074ae60678c1389b4e96a4045b5b
MosesDerProphet <99213944+PatderCoole@users.noreply.github.com>
2022-04-16 00:22:48 +0200

    quasi das dummy Projekt beendet
    Ayy Jo, voll fett abgeschlossen ALta!!!
    - MosesDerProphet O_O

---
+++ Assets/Scripts/KAMERA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KAMERA : MonoBehaviour
+{
+
+    public float rot_mult = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log(Input.GetAxis("Mouse X").ToString());
+            transform.Rotate(new Vector3(0f, Input.GetAxis("Mouse X"),0f) * rot_mult);
+        }
+    }
+}
---
+++ Assets/Scripts/The_Death_Situation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class The_Death_Situation : MonoBehaviour
+{
+
+    public float respawn_hight = -10;
+    public Transform spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < respawn_hight)
+        {
+            respawn();
+        }
+    }
+
+    public void respawn()
+    {
+        transform.position = spawn.position;
+    }
+}
