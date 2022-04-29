634e22a4a8e83b2539d1c348e0e2f36e827c86db
H4x0r-000 <76412429+H4x0r-000@users.noreply.github.com>
2022-04-29 23:16:24 +0200

    final commit
    added win screen
    fixed some issues with broken mat
    added 4th lvl to main menu

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private bool lateStart = true;
+
+    private void Update()
+    {
+        if(lateStart == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            lateStart = false;
+        }
+    }
+
     public void ChangeScene(string lvl)
     {
         SceneManager.LoadScene(lvl);
